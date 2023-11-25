9bf18aa51c4db1b6b48732b05251eabce4e9721c
anishkumar1412 <anishkumarcbsa@gmail.com>
2023-11-25 14:21:21 +0530

    first commit

---
+++ babu.py
@@ -0,0 +1,9 @@
+# Given values
+increased_strength = 1744
+increase_percentage = 9
+
+# Calculate the original strength
+original_strength = increased_strength / (1 + (increase_percentage / 100))
+
+# Print the original strength
+print("The original strength of the school is:", int(original_strength))
---
+++ tut53.js
@@ -0,0 +1,28 @@
+
+function greet(name,greettext="greetings from javascript")
+
+{
+
+
+    let name1="name1";
+console.log(greettext + " " + name);
+    console.log(name + " is a good boy");
+}
+
+console.log("this is tut 52");
+let name = "Harry";
+let name1 = "Shubham";
+let name2 = "Rohan";
+let name3 = "Sagar";
+let greettext = "Good Morning";
+
+greet(name );
+greet(name1 ,greettext);
+greet(name2 ,greettext);
+greet(name3 ,greettext);
+let returnVal = greet(name3);
+console.log(returnVal);
+// console.log(name + " is a good boy");
+// console.log(name2 + " is a good boy");
+// console.log(name3 + " is a good boy");
+// console.log(name1 + " is a good boy");
